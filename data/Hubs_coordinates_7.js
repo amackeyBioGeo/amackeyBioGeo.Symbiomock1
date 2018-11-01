6e70eedba77befd2567fa0c815a967a407f6419e
amackeyBioGeo <40256471+amackeyBioGeo@users.noreply.github.com>
2018-11-01 15:29:27 +1000

    Add files via upload

---
+++ data/Hubs_coordinates_7.js
@@ -0,0 +1,13 @@
+var json_Hubs_coordinates_7 = {
+"type": "FeatureCollection",
+"name": "Hubs_coordinates_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Hub": "Ewen Maddock Dam Water Treatment Plant", "Lon": 152.9926839, "Lat": -26.7820064 }, "geometry": { "type": "Point", "coordinates": [ 152.9926839, -26.7820064 ] } },
+{ "type": "Feature", "properties": { "Hub": "Capalaba Water Treatment Plant", "Lon": 153.18382, "Lat": -27.532602 }, "geometry": { "type": "Point", "coordinates": [ 153.18382, -27.532602 ] } },
+{ "type": "Feature", "properties": { "Hub": "Mt. Crosby Water Treatment Plant", "Lon": 152.7958996, "Lat": -27.537335 }, "geometry": { "type": "Point", "coordinates": [ 152.795899600000013, -27.537335 ] } },
+{ "type": "Feature", "properties": { "Hub": "North Pine Dam", "Lon": 152.9434153, "Lat": -27.2648542 }, "geometry": { "type": "Point", "coordinates": [ 152.9434153, -27.2648542 ] } },
+{ "type": "Feature", "properties": { "Hub": "Hinze Dam", "Lon": 153.2790734, "Lat": -28.0496716 }, "geometry": { "type": "Point", "coordinates": [ 153.279073399999987, -28.0496716 ] } },
+{ "type": "Feature", "properties": { "Hub": "Icon", "Lon": 152.759566, "Lat": -27.6141853 }, "geometry": { "type": "Point", "coordinates": [ 152.759566, -27.6141853 ] } }
+]
+}
---
+++ data/symbio_marker_9.js
@@ -0,0 +1,8 @@
+var json_symbio_marker_9 = {
+"type": "FeatureCollection",
+"name": "symbio_marker_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "Testing Lab": "Symbio", "Postcode": 4113.0, "Lat": -27.57913, "Long": 153.0997124 }, "geometry": { "type": "Point", "coordinates": [ 153.099712399999987, -27.57913 ] } }
+]
+}
